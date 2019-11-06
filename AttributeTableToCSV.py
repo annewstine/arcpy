8f361e7f526a94083ac0b63b80b10d7a5a6ac8c4
annewstine <53316530+annewstine@users.noreply.github.com>
2019-11-06 14:18:08 -0600

    Update AttributeTableToCSV.py

--- AttributeTableToCSV.py
+++ AttributeTableToCSV.py
@@ -3,28 +3,34 @@
 # import modules
 import arcpy, csv
 
-# Setting geoprocessing environments
-arcpy.env.workspace = '/working.gdb'
+#Setting geoprocessing environments
+arcpy.env.workspace = r'C:/Users/AStine/Documents/ArcGIS/test.gdb'
 arcpy.env.overwriteOutput = True
 
-# Local variables
-inTable = 'working.gdb/YourFile'
-rows = arcpy.SearchCursor(inTable)
-outTable = csv.writer(open('C:/output.csv','w', newline=''))
-fields = arcpy.ListFields(inTable)
-fieldnames = [field.name for field in fields if field.type != 'Geometry'] #excludes column with geometry information
+#Define the function
 
-# Use SearchCursor to create your table
-allRows = []
-for row in rows:
-    rowlist = []
-    for field in fieldnames:
-        rowlist.append(row.getValue(field))
-    allRows.append(rowlist)
+def attributeToCsv(inTable, outPath):
+    #define variables
+    rows = arcpy.SearchCursor(inTable)
+    outTable = csv.writer(open((outPath),'w', newline=''))
+    fields = arcpy.ListFields(inTable)
+    fieldnames = [field.name for field in fields if field.type != 'Geometry'] #excludes column with geometry information
 
-# Write your table to desired location.
-outTable.writerow(fieldnames)
-for row in allRows:
-    outTable.writerow(row)
-print('CSV created.')
-outTable.close()
+    # Use SearchCursor to create your table
+    allRows = []
+    for row in rows:
+        rowlist = []
+        for field in fieldnames:
+            rowlist.append(row.getValue(field))
+        allRows.append(rowlist)
+
+    # Write your table to desired location.
+    outTable.writerow(fieldnames)
+    for row in allRows:
+        outTable.writerow(row)
+    print(outPath + ' created.')
+
+inTable = 'C:/Users/AStine/Documents/ArcGIS/test.gdb/parcel_ownership'
+outPath = 'C:/Users/Astine/Documents/test.csv'
+
+attributeToCsv(inTable, outPath)
