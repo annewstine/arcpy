7a70bc426aa8f2deadd01b67a186afcde0f3ad38
annewstine <53316530+annewstine@users.noreply.github.com>
2019-11-08 15:31:51 -0600

    Update IMPROVED_AGO_Info_to_CSV_with_Readable_Dates.py

--- IMPROVED_AGO_Info_to_CSV_with_Readable_Dates.py
+++ IMPROVED_AGO_Info_to_CSV_with_Readable_Dates.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 #import library
 from arcgis import GIS
 import pandas as pd
@@ -11,30 +8,15 @@ from datetime import datetime
 import numpy as np
 
 
-# In[ ]:
-
-
 #connect to the organization
 #replace "USERNAME" with an AGO user
 #you can add ,"PASSWORD" if you dont want to type your password when the script is run (not recommended)
-gis = GIS("https://cedarpark.maps.arcgis.com","Anne.Stine_CedarPark")
-
-
-# In[ ]:
+gis = GIS("https://YOURPAGE.maps.arcgis.com","USERNAME")
 
 
 #define variables for writing item info to file
 key_list = ['id','owner','created','modified','title','type','url','numViews']
-
-
-# In[ ]:
-
-
 all_items = gis.content.search('*',max_items=400)
-all_keys = list(all_items[0].keys())
-
-
-# In[ ]:
 
 
 #Write data to list of lists
@@ -46,19 +28,10 @@ for x in range(len(all_items)):
         value_list.append(value)
     bigList.append(value_list)
     
-        
-
-
-# In[ ]:
-
-
+    
 #Make array of list of lists, build dataframe from array
 data = np.array(bigList)
 df = pd.DataFrame(data=data, columns=key_list)        
-df
-
-
-# In[ ]:
 
 
 #Convert timestamps to human-readable dates
@@ -66,9 +39,6 @@ df['DATEcreated']=(pd.to_datetime(df.created,unit='ms'))
 df['DATEmod']=(pd.to_datetime(df.modified,unit='ms'))
 
 
-# In[ ]:
-
-
-#Export df_out to csv
+#Export dataframe to csv
 df.to_csv (r'C:\Users\Astine\desktop\test.csv', index = None, header=True) 
 
